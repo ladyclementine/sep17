9de03df7073991a8f7ca92dc598e18ab1cd38a5d
Vicente Lucas <vica.escorcio@gmail.com>
2016-07-15 00:20:13 -0300

    confirmable(migracao feita, mas nao utilizado). Autenticao feita com devise. Controller profiles

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,12 @@
     end
   end
 
+
+
 Rails.application.routes.draw do
- 
- 
+ devise_for :users, controllers: { sessions: 'users/sessions', confirmations: 'users/confirmations', passwords: 'users/passwords', registrations:'users/registrations', unlocks:'users/unlocks', omniauth:'users/omniauths'}
+ get 'profiles/index'
+root "profiles#index", as: :authenticate_root 
 
   constraints SubdomainConstraint do
    
