955c8958071c99c0cb2e5e30d0e5f3169b4f3d16
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2016-07-25 00:22:59 -0300

    Admin views-responsive

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,19 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require core/modernizr.min
+//= require jquery2
 //= require jquery_ujs
 //= require turbolinks
 //= require core/bootstrap
+//= require core/detect
+//= require core/fastclick
+//= require core/jquery.blockUI
+//= require core/waves
+//= require core/jquery.nicescroll
+//= require core/jquery.slimscroll
+//= require core/jquery.scrollTo.min
+//= require core/jquery.core
 //= require core/jquery.app
 //= require plugins/datatables/jquery.dataTables.min
 //= require plugins/datatables/dataTables.bootstrap
@@ -31,4 +40,35 @@
 
 jQuery(window).bind("load", function() {
   $('#datatable').dataTable();
+
+  var openLeftBtn = $(".open-left");
+
+  function openLeftBar() {
+    $("#wrapper").toggleClass("enlarged");
+    $("#wrapper").addClass("forced");
+
+    if ($("#wrapper").hasClass("enlarged") && $("body").hasClass("fixed-left")) {
+      $("body").removeClass("fixed-left").addClass("fixed-left-void");
+    } else if (!$("#wrapper").hasClass("enlarged") && $("body").hasClass("fixed-left-void")) {
+      $("body").removeClass("fixed-left-void").addClass("fixed-left");
+    }
+
+    if ($("#wrapper").hasClass("enlarged")) {
+      $(".left ul").removeAttr("style");
+    } else {
+      $(".subdrop").siblings("ul:first").show();
+    }
+
+    //this.toggle_slimscroll(".slimscrollleft");
+    $("body").trigger("resize");
+    // for mobile screen re-intializing it
+    if(jQuery.browser.mobile === true) {
+      $('.slimscrollleft').getNiceScroll().resize();
+    }
+  }
+  openLeftBtn.on("click", function (e) {
+    openLeftBar();
+  });
 });
+
+var resizefunc = [];
