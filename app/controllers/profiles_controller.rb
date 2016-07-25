fb514308667a15c9ae60b7432b6438b33f5d8eeb
Vicente Lucas <vica.escorcio@gmail.com>
2016-07-25 13:56:53 -0300

    views week e weekUser

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,17 @@
 class ProfilesController < ApplicationController
 	before_action :authenticate_user!
 	layout 'profile_layout'
+
+	
   def index
   	@user = current_user
   end
+
+  def week
+@user = current_user
+  end
+
+  def weekUser
+  end
+
 end
