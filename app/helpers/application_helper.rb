51b4f636a2ce4e4ab94d0cae4a9e5324372f56cd
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2016-09-14 09:01:38 -0300

    weeks helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,13 @@ module ApplicationHelper
   def current_week
     case Rails.application.secrets.week_id
     when "semana_test"
-      @week = {name: 'Semana em Teste', sigla: 'SeT', colors: [base: '#FFFFFF'], logo: 'semana_test-logo.png'}
+      @week = { name: 'Semana em Teste', sigla: 'SeT', colors: [base: '#FFFFFF'], logo: 'semana_test-logo.png' }
+    when "stem"
+      @week = { name: 'Semana da Emgenharia Mecânica', sigla: 'STEM', colors: [base: '#FFFFFF'], logo: 'stem-logo.png' }
+    when "sec"
+      @week = { name: 'Semana da Emgenharia Civil', sigla: 'SEC', colors: [base: '#FFFFFF'], logo: 'sec-logo.png' }
+    when "setic"
+      @week = { name: 'Semana de Tecnologia e Inovação', sigla: 'Setic', colors: [base: '#FFFFFF'], logo: 'setic-logo.png' }
     else
       @week = {}
     end
