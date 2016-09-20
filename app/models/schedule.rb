0e065940c7f7c606baea1c5848cd8bc64f2ccc96
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2016-09-20 13:20:37 -0300

    Schedule methods

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -5,12 +5,27 @@ class Schedule < ActiveRecord::Base
 
   validates_datetime :end_time, after: :start_time
 
-  def is_between?(reference)
-    between_comp = reference.start_time.between?(self.start_time,self.end_time) ||
-      reference.end_time.between?(self.start_time,self.end_time)
-    equal_comp = reference.start_time == self.start_time ||
-      reference.end_time == self.end_time
+  def start_time_is_between?
+    begin
+      schedules = Schedule
+        .where.not(id: self.id)
+        .where('? BETWEEN start_time and end_time', self.start_time)
+      raise unless schedules.any?
+      []
+    rescue => e
+      [self.start_time]
+    end
+  end
 
-    between_comp || equal_comp
+  def end_time_is_between?
+    begin
+      schedules = Schedule
+        .where.not(id: self.id)
+        .where('? BETWEEN start_time and end_time', self.end_time)
+      raise unless schedules.any?
+      []
+    rescue => e
+      [self.end_time]
+    end
   end
 end
