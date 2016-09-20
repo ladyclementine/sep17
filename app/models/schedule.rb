6e3266887a9d782388d03e72b753763daad47398
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2016-09-20 17:14:05 -0300

    Schedule diff times

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -5,27 +5,29 @@ class Schedule < ActiveRecord::Base
 
   validates_datetime :end_time, after: :start_time
 
-  def start_time_is_between?
+  def start_time_between
     begin
       schedules = Schedule
         .where.not(id: self.id)
         .where('? BETWEEN start_time and end_time', self.start_time)
+      byebug
       raise unless schedules.any?
-      []
+      [schedules]
     rescue => e
-      [self.start_time]
+      []
     end
   end
 
-  def end_time_is_between?
+  def end_time_between
     begin
       schedules = Schedule
         .where.not(id: self.id)
         .where('? BETWEEN start_time and end_time', self.end_time)
       raise unless schedules.any?
-      []
+      [schedules]
     rescue => e
-      [self.end_time]
+      []
     end
   end
+
 end
