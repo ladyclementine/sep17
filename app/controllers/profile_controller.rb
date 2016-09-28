0e7a4c7859da20bc3116f353bf1989a641bebc3f
Vicente Lucas <vica.escorcio@gmail.com>
2016-09-28 13:59:50 -0300

    ajeitando verify_coucart

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -33,7 +33,7 @@ class ProfileController < ApplicationController
         redirect_to :events, notice:"adicione #{lectures} palestras ao seu carrinho para completar seu pacote"
       elsif count[:visits]<package.visits
         redirect_to :events, notice: " adicione #{visits} visitas ao seu carrinho para completar o pacote"
-      elsif count[:courses]<package.visits
+      elsif count[:courses]<package.courses
         redirect_to :events, notice: " adicione #{courses} mini-cursos ao seu carrinho para completar o pacote"
       end
     end
