18f10df66a044ee4a6e86bf7607ea62f634afc9c
Vicente Lucas <vica.escorcio@gmail.com>
2016-10-10 23:46:45 -0300

    adição de eventos e remoção na variável do redis

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,6 @@ class EventsController < ProfileController
     @eventsDays = Event.days
     @scheduleHash = Event.appointments
     @my_events = @user.get_cart_events
-    byebug
     @my_events_day = Event.appointment(@my_events)
     @number = 0
   end
