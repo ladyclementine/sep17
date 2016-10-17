2371c188820b3d1f8951a2e1cfac7946a8caa4f6
Vicente Lucas <vica.escorcio@gmail.com>
2016-10-17 09:54:41 -0300

    mudança no redis

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ProfileController
     @events = Event.all
     @eventsDays = Event.days
     @scheduleHash = Event.appointments
-    @my_events = @user.get_cart_events
+    @my_events = @user.events
     @my_events_day = Event.appointment(@my_events)
     @number = 0
   end
