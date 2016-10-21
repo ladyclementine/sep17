c165aec03b01b826cfd23e0ac330021339b59289
rodrigotnoronha <rodrigotnoronha@gmail.com>
2016-10-20 22:05:50 -0300

    Assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,8 +41,6 @@
 //= require plugins/datatables/dataTables.scroller.min
 //= require plugins/toastr.min.js
 //= require carts.js
-//= require mask.js
-//= require rg_mask.js
 
 jQuery(document).ready(function() {
   $('[data-form-prepend]').click(function(e) {
